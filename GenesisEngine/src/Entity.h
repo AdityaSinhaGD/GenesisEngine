4595604f83b4e0dd0a9e2c8d1badabf39ec22872
DESKTOP-1PFIG8A\Aditya Sinha <sinha.aditya02@gmail.com>
2021-02-28 22:38:06 -0500

    implemented map to track adding components. During component addition the type is added to our map so it be retrieved later.

--- GenesisEngine/src/Entity.h
+++ GenesisEngine/src/Entity.h
@@ -25,14 +25,21 @@ public:
 	void Destroy();
 	bool IsActive() const;
 
-	template <typename T, typename... TArgs>
+	template<typename T, typename... TArgs>
 	T& AddComponent(TArgs&&... args)
 	{
 		T* newComponent(new T(std::forward<TArgs>(args)...));
 		newComponent->owner = this;
 		components.emplace_back(newComponent);
+		componentTypeMap[&typeid(newComponent)] = newComponent;
 		newComponent->Initialize();
 		return *newComponent;
+	}
+
+	template<typename T>
+	T* GetComponent()
+	{
+		return static_cast<T*>(componentTypeMap[&typeid(T)]);
 	}
 
 };
