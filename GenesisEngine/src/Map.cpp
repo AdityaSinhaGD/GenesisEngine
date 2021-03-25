3084328f971448d75516745efd3cd9d349305512
DESKTOP-1PFIG8A\Aditya Sinha <sinha.aditya02@gmail.com>
2021-03-24 20:31:51 -0400

    Basic map class created to add a tilemap component.

---
+++ GenesisEngine/src/Map.cpp
@@ -0,0 +1,17 @@
+#include "Map.h"
+
+Map::Map(std::string textureId, int scale, int tileSize)
+{
+}
+
+Map::~Map()
+{
+}
+
+void Map::LoadMap(std::string filePath, int mapSizeX, int mapSizeY)
+{
+}
+
+void Map::AddTile(int sourceRectX, int sourceRectY, int x, int y)
+{
+}
