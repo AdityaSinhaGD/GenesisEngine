b7312537b72d471b050f526a8946676472d55652
DESKTOP-1PFIG8A\Aditya Sinha <sinha.aditya02@gmail.com>
2021-03-24 21:16:53 -0400

    Basic TileComponent class created. AddTile functionality implemented in Map.cpp

---
+++ GenesisEngine/src/Components/TileComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <SDL.h>
+#include "../libs/glm/glm.hpp"
+#include "../EntityManager.h"
+#include "../AssetManager.h"
+
+class TileComponent :public Component
+{
+public:
+	SDL_Texture* texture;
+	SDL_Rect sourceRectangle;
+	SDL_Rect targetRectangle;
+	glm::vec2 position;
+
+	TileComponent(int sourceRectX, int sourceRectY, int x, int y, int tileSize, int tileScale, std::string assetTextureId)
+	{
+		texture = Game::assetManager->GetTexture(assetTextureId);
+
+		sourceRectangle.x = sourceRectX;
+		sourceRectangle.y = sourceRectY;
+		sourceRectangle.w = tileSize;
+		sourceRectangle.h = tileSize;
+
+		targetRectangle.x = x;
+		targetRectangle.y = y;
+		targetRectangle.w = tileSize * tileScale;
+		targetRectangle.h = tileSize * tileScale;
+
+		position.x = x;
+		position.y = y;
+	}
+
+	~TileComponent()
+	{
+		SDL_DestroyTexture(texture);
+	}
+
+	//Update tile positions based on Camera values.
+	void Update(float deltaTime) override
+	{
+
+	}
+
+	void Render() override
+	{
+		TextureManager::DrawTexture(texture, sourceRectangle, targetRectangle, SDL_FLIP_NONE);
+	}
+};
--- GenesisEngine/src/Map.cpp
+++ GenesisEngine/src/Map.cpp
@@ -1,6 +1,10 @@
 #include "Map.h"
+#include<fstream>
 #include "Game.h"
 #include "EntityManager.h"
+#include "Components/TileComponent.h"
+
+extern EntityManager manager;
 
 Map::Map(std::string textureId, int scale, int tileSize)
 {
@@ -17,11 +21,13 @@ Map::~Map()
 //Read map defined in text file and use it to load a grid of tiles
 void Map::LoadMap(std::string filePath, int mapSizeX, int mapSizeY)
 {
-	
+	std::fstream mapFile;
+	mapFile.open(filePath);
 }
 
 //Add new tile obj to the game scene
 void Map::AddTile(int sourceRectX, int sourceRectY, int x, int y)
 {
-
+	Entity& newTile(manager.AddEntity("Tile")); //This will break the unique entitiy naming scheme for tile type but I can live with this for now.
+	newTile.AddComponent<TileComponent>(sourceRectX, sourceRectY, x, y, scale, textureId);
 }
