2936b5cadf50eb50499dd2c2737a87667f096774
DESKTOP-1PFIG8A\Aditya Sinha <sinha.aditya02@gmail.com>
2021-03-24 20:45:54 -0400

    Map class wip.

--- GenesisEngine/src/Map.cpp
+++ GenesisEngine/src/Map.cpp
@@ -1,17 +1,27 @@
 #include "Map.h"
+#include "Game.h"
+#include "EntityManager.h"
 
 Map::Map(std::string textureId, int scale, int tileSize)
 {
+	this->textureId = textureId;
+	this->scale = scale;
+	this->tileSize = tileSize;
 }
 
 Map::~Map()
 {
+	
 }
 
+//Read map defined in text file and use it to load a grid of tiles
 void Map::LoadMap(std::string filePath, int mapSizeX, int mapSizeY)
 {
+	
 }
 
+//Add new tile obj to the game scene
 void Map::AddTile(int sourceRectX, int sourceRectY, int x, int y)
 {
+
 }
